8d389c2fa4d1ae03085f8d606825afb067314078
AaronCasildo <aacasildor@gmail.com>
2025-10-16 18:42:16 -0700

    Added running instruction for the backend service

--- Backend/main.py
+++ Backend/main.py
@@ -1,3 +1,5 @@
+#to run the backend server, use the command: uvicorn main:app --reload
+
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
